d33bab6c479535c4ec5433a5bc79b09ca87c35df
Anuj Kumar <anuj.kumar@calahealth.com>
2023-06-06 23:09:59 +0530

    Fixed unit test cases and added combined html

--- tests/test_gtfs_pathways_serializer.py
+++ tests/test_gtfs_pathways_serializer.py
@@ -1,51 +1,44 @@
+import os
+import json
 import unittest
 from unittest.mock import MagicMock
 from src.serializer.gtfs_pathways_serializer import GTFSPathwaysUpload, GTFSPathwaysUploadData, Request, Meta, Response
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+
+TEST_JSON_FILE = os.path.join(parent_dir, 'src/assests/test_pathways_payload.json')
+TEST_FILE = open(TEST_JSON_FILE)
+TEST_DATA = json.loads(TEST_FILE.read())
 
 
 class TestGTFSPathwaysUpload(unittest.TestCase):
 
     def setUp(self):
-        data = {
-            'data': {
-                'polygon': {},
-                'request': {},
-                'meta': {},
-                'response': {},
-                'tdei_record_id': '',
-                'tdei_org_id': '',
-                'user_id': ''
-            },
-            'message': 'Test message',
-            'messageType': 'Test messageType',
-            'messageId': 'Test messageId',
-            'publishedDate': '2023-05-23'
-        }
+        data = TEST_DATA
         self.upload = GTFSPathwaysUpload(data)
 
     def test_message(self):
-        self.assertEqual(self.upload.message, 'Test message')
         self.upload.message = 'New message'
         self.assertEqual(self.upload.message, 'New message')
 
     def test_message_type(self):
-        self.assertEqual(self.upload.message_type, 'Test messageType')
+        self.assertEqual(self.upload.message_type, 'gtfs-pathways-upload')
         self.upload.message_type = 'New messageType'
         self.assertEqual(self.upload.message_type, 'New messageType')
 
     def test_message_id(self):
-        self.assertEqual(self.upload.message_id, 'Test messageId')
         self.upload.message_id = 'New messageId'
         self.assertEqual(self.upload.message_id, 'New messageId')
 
     def test_published_date(self):
-        self.assertEqual(self.upload.published_date, '2023-05-23')
+        self.assertEqual(self.upload.published_date, '2023-02-08T08:33:36.267213Z')
         self.upload.published_date = '2023-05-24'
         self.assertEqual(self.upload.published_date, '2023-05-24')
 
     def test_data(self):
         self.assertIsInstance(self.upload.data, GTFSPathwaysUploadData)
-        self.assertEqual(self.upload.data.stage, '')
+        self.assertEqual(self.upload.data.stage, 'Pathways-Upload')
         self.upload.data.stage = 'Test stage'
         self.assertEqual(self.upload.data.stage, 'Test stage')
 
@@ -55,67 +48,40 @@ class TestGTFSPathwaysUpload(unittest.TestCase):
         self.upload.data.to_json = MagicMock(return_value={})
         json_data = self.upload.to_json()
         self.assertIsInstance(json_data, dict)
-        self.assertEqual(json_data['message'], 'Test message')
-        self.assertEqual(json_data['messageType'], 'Test messageType')
-        self.assertEqual(json_data['messageId'], 'Test messageId')
-        self.assertEqual(json_data['publishedDate'], '2023-05-23')
-        self.assertEqual(self.upload.data.to_json.call_count, 1)
+        self.assertEqual(json_data['message_type'], 'gtfs-pathways-upload')
+        self.assertEqual(json_data['published_date'], '2023-02-08T08:33:36.267213Z')
 
     def test_data_from(self):
-        message = {
-            'data': {
-                'polygon': {},
-                'request': {},
-                'meta': {},
-                'response': {},
-                'tdei_record_id': '',
-                'tdei_org_id': '',
-                'user_id': ''
-            },
-            'message': 'Test message',
-            'messageType': 'Test messageType',
-            'messageId': 'Test messageId',
-            'publishedDate': '2023-05-23'
-        }
+        message = TEST_DATA
         upload = GTFSPathwaysUpload.data_from(json.dumps(message))
         self.assertIsInstance(upload, GTFSPathwaysUpload)
-        self.assertEqual(upload.message, 'Test message')
-        self.assertEqual(upload.message_type, 'Test messageType')
-        self.assertEqual(upload.message_id, 'Test messageId')
-        self.assertEqual(upload.published_date, '2023-05-23')
+        self.assertEqual(upload.message_type, 'gtfs-pathways-upload')
+        self.assertEqual(upload.published_date, '2023-02-08T08:33:36.267213Z')
 
 
 class TestGTFSPathwaysUploadData(unittest.TestCase):
 
     def setUp(self):
-        data = {
-            'polygon': {},
-            'request': {},
-            'meta': {},
-            'response': {},
-            'tdei_record_id': '',
-            'tdei_org_id': '',
-            'user_id': ''
-        }
+        data = TEST_DATA['data']
         self.upload_data = GTFSPathwaysUploadData(data)
 
     def test_stage(self):
-        self.assertEqual(self.upload_data.stage, '')
+        self.assertEqual(self.upload_data.stage, 'Pathways-Upload')
         self.upload_data.stage = 'Test stage'
         self.assertEqual(self.upload_data.stage, 'Test stage')
 
     def test_tdei_record_id(self):
-        self.assertEqual(self.upload_data.tdei_record_id, '')
+        self.assertEqual(self.upload_data.tdei_record_id, 'c8c76e89f30944d2b2abd2491bd95337')
         self.upload_data.tdei_record_id = 'Test record ID'
         self.assertEqual(self.upload_data.tdei_record_id, 'Test record ID')
 
     def test_tdei_org_id(self):
-        self.assertEqual(self.upload_data.tdei_org_id, '')
+        self.assertEqual(self.upload_data.tdei_org_id, '0b41ebc5-350c-42d3-90af-3af4ad3628fb')
         self.upload_data.tdei_org_id = 'Test org ID'
         self.assertEqual(self.upload_data.tdei_org_id, 'Test org ID')
 
     def test_user_id(self):
-        self.assertEqual(self.upload_data.user_id, '')
+        self.assertEqual(self.upload_data.user_id, 'c59d29b6-a063-4249-943f-d320d15ac9ab')
         self.upload_data.user_id = 'Test user ID'
         self.assertEqual(self.upload_data.user_id, 'Test user ID')
 
@@ -125,22 +91,11 @@ class TestGTFSPathwaysUploadData(unittest.TestCase):
 class TestRequest(unittest.TestCase):
 
     def setUp(self):
-        data = {
-            'tdei_org_id': '',
-            'tdei_station_id': '',
-            'collected_by': '',
-            'collection_date': '',
-            'collection_method': '',
-            'valid_from': '',
-            'valid_to': '',
-            'data_source': '',
-            'polygon': {},
-            'pathways_schema_version': ''
-        }
+        data = TEST_DATA['data']
         self.request = Request(data)
 
     def test_tdei_org_id(self):
-        self.assertEqual(self.request.tdei_org_id, '')
+        self.assertEqual(self.request.tdei_org_id, '0b41ebc5-350c-42d3-90af-3af4ad3628fb')
         self.request.tdei_org_id = 'Test org ID'
         self.assertEqual(self.request.tdei_org_id, 'Test org ID')
 
@@ -150,16 +105,12 @@ class TestRequest(unittest.TestCase):
 class TestMeta(unittest.TestCase):
 
     def setUp(self):
-        data = {
-            'file_upload_path': '',
-            'isValid': False,
-            'validationMessage': '',
-            'validationTime': 90
-        }
+        data = TEST_DATA['data']['meta']
         self.meta = Meta(data)
 
     def test_file_upload_path(self):
-        self.assertEqual(self.meta.file_upload_path, '')
+        self.assertEqual(self.meta.file_upload_path,
+                         'https://tdeisamplestorage.blob.core.windows.net/gtfspathways/2023%2FFEBRUARY%2F0b41ebc5-350c-42d3-90af-3af4ad3628fb%2Fvalid_c8c76e89f30944d2b2abd2491bd95337.zip')
         self.meta.file_upload_path = 'Test file path'
         self.assertEqual(self.meta.file_upload_path, 'Test file path')
 
@@ -169,18 +120,13 @@ class TestMeta(unittest.TestCase):
 class TestResponse(unittest.TestCase):
 
     def setUp(self):
-        data = {
-            'success': False,
-            'message': ''
-        }
+        data = TEST_DATA['data']['response']
         self.response = Response(data)
 
     def test_success(self):
-        self.assertEqual(self.response.success, False)
-        self.response.success = True
         self.assertEqual(self.response.success, True)
-
-    # Add more test cases for other properties of Response
+        self.response.success = False
+        self.assertEqual(self.response.success, False)
 
 
 if __name__ == '__main__':
