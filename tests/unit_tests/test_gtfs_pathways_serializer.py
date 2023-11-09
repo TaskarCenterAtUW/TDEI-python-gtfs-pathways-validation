35bc7664c09e0493d18e8351679668402325e783
Achyut Kumar M <achyutkumar.m@gmail.com>
2023-11-09 20:51:05 +0530

    rename org to project group

--- tests/unit_tests/test_gtfs_pathways_serializer.py
+++ tests/unit_tests/test_gtfs_pathways_serializer.py
@@ -78,8 +78,8 @@ class TestGTFSPathwaysUploadData(unittest.TestCase):
 
     def test_tdei_project_group_id(self):
         self.assertEqual(self.upload_data.tdei_project_group_id, '0b41ebc5-350c-42d3-90af-3af4ad3628fb')
-        self.upload_data.tdei_project_group_id = 'Test org ID'
-        self.assertEqual(self.upload_data.tdei_project_group_id, 'Test org ID')
+        self.upload_data.tdei_project_group_id = 'Test Project Group ID'
+        self.assertEqual(self.upload_data.tdei_project_group_id, 'Test Project Group ID')
 
     def test_user_id(self):
         self.assertEqual(self.upload_data.user_id, 'c59d29b6-a063-4249-943f-d320d15ac9ab')
@@ -97,8 +97,8 @@ class TestRequest(unittest.TestCase):
 
     def test_tdei_project_group_id(self):
         self.assertEqual(self.request.tdei_project_group_id, '0b41ebc5-350c-42d3-90af-3af4ad3628fb')
-        self.request.tdei_project_group_id = 'Test org ID'
-        self.assertEqual(self.request.tdei_project_group_id, 'Test org ID')
+        self.request.tdei_project_group_id = 'Test Project Group ID'
+        self.assertEqual(self.request.tdei_project_group_id, 'Test Project Group ID')
 
     # Add more test cases for other properties of Request
 
